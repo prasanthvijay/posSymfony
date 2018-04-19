da11d85c327fee768d0a2f04b47fb11b0977c86e
prasanth <prasanth.s@pickzy.com>
2018-04-19 18:52:48 +0530

    Header

--- src/PosBundle/Controller/DefaultController.php
+++ src/PosBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PosBundle:Default:index.html.twig');
+        return $this->render('PosBundle:Pos:index.html.twig');
     }
 }
